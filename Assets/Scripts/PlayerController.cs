54f6a9aa7a99c7758e1815d9ee3a34c55ac44f3b
Maxime Morin <maxime_morette@hotmail.com>
2022-04-04 15:47:42 -0400

    NOW WITH CROSSHAIR!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -14,7 +15,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float runningSpeed = 4.0f;
     [SerializeField] private float jumpHeight = 5.0f;
     [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private float rotationSpeed = 240f;
     [SerializeField] private Transform cam;
+    [SerializeField] private GameObject aimTargetFollow;
+    [SerializeField] private Image crosshair;
 
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
 
@@ -64,11 +68,39 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetButton("Fire2"))
         {
+            aimTargetFollow.SetActive(true);
             aimVirtualCamera.gameObject.SetActive(true);
+            crosshair.gameObject.SetActive(true);
         }
         else
         {
+            aimTargetFollow.SetActive(false);
             aimVirtualCamera.gameObject.SetActive(false);
+            crosshair.gameObject.SetActive(false);
+            aimTargetFollow.transform.rotation = transform.rotation;
+        }
+
+        if (aimVirtualCamera.gameObject.activeInHierarchy)
+        {
+            float v = Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+            float h = Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+
+            if ((v < 0 || v > 0) && h == 0)
+            {
+                Vector3 dir = new Vector3(0f, v);
+
+                transform.Rotate(dir.normalized * Time.deltaTime * rotationSpeed);
+            }
+
+            if ((h < 0 || h > 0) && v == 0)
+            {
+                Vector3 dir = new Vector3(-h, 0f);
+
+                aimTargetFollow.transform.Rotate(dir.normalized * Time.deltaTime * rotationSpeed);
+            }
+            
+            
+            
         }
 
     }
