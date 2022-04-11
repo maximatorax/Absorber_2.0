9bdd9f05ce94c962a7bbf979c3703b41b0eb192c
Maxime Morin <maxime_morette@hotmail.com>
2022-04-11 02:16:40 -0400

    Functioning healthbar on top of enemies head and fixed the camera stuterring bug when walking backwards

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
     [SerializeField] private int currentHealth;
     [SerializeField] private int maxHealth = 100;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private Transform faceTowardTarget;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.minValue = 0;
+        healthBar.maxValue = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthBar.value = currentHealth;
+        if (gameObject.tag == "Enemy")
+        {
+            healthBar.transform.LookAt(faceTowardTarget);
+        }
+    }
+
+    public void CalculateHealthBarColor()
+    {
+        healthBar.targetGraphic.color = Color.Lerp(Color.red, Color.green, healthBar.value / healthBar.maxValue);
     }
 
     public void TakeDamage(int damage)
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -44,8 +44,12 @@ public class PlayerController : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, 
                 ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            if (!aimVirtualCamera.gameObject.activeInHierarchy)
+            {
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            }
+            
             Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
             if (Input.GetButton("Sprint"))
